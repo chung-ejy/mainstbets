4eb4bb897457891d7125c0349e539ff7c1839b32
chung-ejy <chung.ejy@gmail.com>
2021-08-26 16:04:46 -0400

    implemented sort parameters

--- mainstbets/frontend/src/components/stock/TimeSeries.js
+++ mainstbets/frontend/src/components/stock/TimeSeries.js
@@ -3,13 +3,33 @@ import StockContext from '../../context/stock/stockContext'
 
 const TimeSeries = ({timeseries}) => {
     const stockContext = useContext(StockContext)
-    const {loading} = stockContext
-    const [state,setState] = useState({"page":0,"ticker":""})
+    const {loading,getStock} = stockContext
+    const [state,setState] = useState({"page":0,"ticker":"","sort":"","order":1})
     const onChange = (e) => {
         e.preventDefault()
         setState({...state,[e.target.name]:e.target.value})
     }
-    const {page,ticker} = state
+    const onClick = (e) => {
+        e.preventDefault()
+        getStock({"ticker":e.target.id})
+    }
+    const defineSort = (e) => {
+        e.preventDefault()
+        console.log(e.target.id)
+        setState({...state,["sort"]:e.target.id,["order"]:order*-1})
+    }
+    const createRow =(stock) => {
+        return (<tr>
+                {Object.keys(stock).map(k => (
+                    k == "ticker" ? <td id={stock[k]} onClick={onClick}>{stock[k]}</td>:<td>{stock[k]}</td>
+                ))}
+            </tr>)
+    }
+    // const onHover = (e) => {
+    //     e.preventDefault()
+    //     console.log(Object.keys(e.target))
+    // }
+    const {page,ticker,sort,order} = state
     return (
         <div className="card card-body mt-4 mb-4">
             {/* <h5 class="card-title text-center mb-1">
@@ -22,28 +42,23 @@ const TimeSeries = ({timeseries}) => {
                         <th>            
                             <form>
                             <div className="form-group-inline">
-                                <input onChange={onChange} className="form-control" 
-                                name="ticker" placeholder="" type="text" value={ticker} />
+                                <input style={{border:0
+                                            ,fontFamily:"inherit"
+                                            ,padding:0
+                                            ,background:"inherit"
+                                        ,outline:"none"}}onChange={onChange} className="form-control" 
+                                name="ticker" placeholder="ticker" type="text" value={ticker} />
                             </div>
                             </form>
                         </th>
-                        <th>Adj Close</th>
-                        <th>Rolling 100</th>
-                        <th>Gainz</th>
+                        <th id="adjClose" onClick={defineSort}>Adj Close</th>
+                        <th id="rolling" onClick={defineSort}>Rolling 100</th>
+                        <th id="gain" onClick={defineSort}>Gainz</th>
                     </tr>
-                {loading ? <tr></tr> : timeseries.filter(ts => ts["ticker"]==ticker).length == 0 ?  timeseries.slice(Number(page)*10,(Number(page)+1)*10).map(stock => (
-                    <tr>
-                    {Object.keys(stock).map(k => (
-                        <td>{stock[k]}</td>
-                    ))}
-                </tr>
-                )): timeseries.filter(ts => ts["ticker"]==ticker).slice(Number(page)*10,(Number(page)+1)*10).map(stock => (
-                    <tr>
-                    {Object.keys(stock).map(k => (
-                        <td>{stock[k]}</td>
-                    ))}
-                </tr>
-                ))}
+                {loading ? <tr></tr> : timeseries.filter(ts => ts["ticker"]==ticker).length == 0 
+                        ?  timeseries.sort((a,b) => (a[sort] - b[sort]) * order).slice(Number(page)*10,(Number(page)+1)*10).map(stock => createRow(stock)) 
+                        : timeseries.sort((a,b) => (a[sort] - b[sort]) * order).filter(ts => ts["ticker"]==ticker).slice(Number(page)*10,(Number(page)+1)*10).map(stock => createRow(stock))
+                        }
                 </tbody>
             </table>
             <form>
