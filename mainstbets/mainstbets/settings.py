0149647f18fcdcfd190dfafda72f3622b3a8022a
chung-ejy <chung.ejy@gmail.com>
2021-08-30 08:26:30 -0400

    deploy settings

--- mainstbets/mainstbets/settings.py
+++ mainstbets/mainstbets/settings.py
@@ -127,3 +127,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+import django_heroku
+django_heroku.settings(locals())
